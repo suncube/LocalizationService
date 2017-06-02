084692b05be68b5c65de489742e4d5bbc05e3f9c
StanislavNos <stanislavnos@gmail.com>
2017-06-02 18:01:15 +0300

    refresh asset

---
+++ Assets/LocalizeService/Plugins/Core/SafetyAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class SafetyAction
+{
+	public static void SafeInvoke(this Action action)
+	{
+		if (action != null)
+		{
+			action.Invoke();
+		}
+	}
+
+	public static void SafeInvoke<T>(this Action<T> action, T arg1)
+	{
+		if (action != null)
+		{
+			action.Invoke(arg1);
+		}
+	}
+
+	public static void SafeInvoke<T1, T2>(this Action<T1, T2> action, T1 arg1, T2 arg2)
+	{
+		if (action != null)
+		{
+			action.Invoke(arg1, arg2);
+		}
+	}
+
+}
