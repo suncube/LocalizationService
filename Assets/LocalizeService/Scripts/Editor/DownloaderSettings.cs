8f9c0181c8d9a491d23df3a223e0730eda6a7d9f
StanislavNos <stanislavnos@gmail.com>
2019-03-15 10:20:56 +0300

    ++ add info

---
+++ Assets/LocalizeService/Scripts/Editor/DownloaderSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownloaderSettings : ScriptableObject
+{
+    public string spreadSheetKey = "";
+    public string loadingDir = "./Assets/LocalizeService/Resources/Localization";
+    public string accessCode = "";
+
+
+    public static DownloaderSettings GetInstance()
+    {
+        return new DownloaderSettings();
+    }
+
+    public void Load()
+    {
+
+    }
+
+    public void Save()
+    {
+
+    }
+}
